5b5631eded910e5b28e8207a9bc843fa6cc774cc
Pelane Sanon <p_sanon@aol.com>
2018-08-22 10:28:53 -0500

    made my jsfile

---
+++ tttpj.js
@@ -0,0 +1,17 @@
+
+class tikTacToe
+    {
+        constructor (xaxis,yaxis)
+        {
+            this.name=xaxis;
+            this.name=yaxis;
+        }
+
+
+    }
+
+
+    function main()
+{
+    
+}
